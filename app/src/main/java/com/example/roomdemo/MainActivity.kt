3dac03814b879e4ab75fa56384a3e097b19641e5
artem1187 <bukinartem11@gmail.com>
2025-11-10 13:20:35 +0700

    Изменение конфигурации сборки

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -16,14 +16,10 @@ import com.example.roomdemo.ui.theme.RoomDemoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             RoomDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ScreenSetup(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +27,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ScreenSetup(modifier: Modifier = Modifier) {
+    MainScreen(modifier)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RoomDemoTheme {
-        Greeting("Android")
-    }
+fun MainScreen(modifier: Modifier = Modifier) {
+
 }
